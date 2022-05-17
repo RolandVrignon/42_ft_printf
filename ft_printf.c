8b870ba44adfd1a144c1d692bc5ffbf2dfe39333
RolandVrignon <roland.vrignon@gmail.com>
2022-05-17 03:01:18 +0200

    minor changes

--- ft_printf.c
+++ ft_printf.c
@@ -6,34 +6,62 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 22:45:05 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/05/13 23:39:59 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/05/17 03:00:10 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-struct t_args
+const char	*get_flag(va_list args, const char *c)
 {
-	char flag;
-	char *content; 
-};
+	if (*c == 'c')
+		printf("Flag = c");
+	else if (*c == 's')
+		printf("Flag = s");
+	else if (*c == 'p')
+		printf("Flag = p");
+	else if (*c == 'd')
+	{
+		char *a = ft_itoa(va_arg(args, int));
+		write(1, &a, ft_strlen(a));
+	}
+	else if (*c == 'i')
+		printf("Flag = i");
+	else if (*c == 'u')
+		printf("Flag = u");
+	else if (*c == 'x')
+		printf("Flag = x");
+	else if (*c == 'X')
+		printf("Flag = X");
+	else if (*c == '%')
+		printf("Flag = Hey");
+	else
+		return (NULL);
+	c++;
+	return (c);
+}
 
-
-int ft_printf(const char *str, ...)
+int ft_printf(const char *format, ...)
 {
 	va_list args;
-	int		i;
-
-	i = 0;
-	va_start(args, str);
-	while (str[i] != '\0')
+	int		len;
+	
+	va_start(args, format);
+	len = 0;
+	while (*format)
 	{
-		if (str[i] == '%' && str[i + 1] != '\0')
-			printf("%d", va_arg(args, char));
+		if (*format == '%')
+			format = get_flag(args, format + 1);
 		else
-			ft_putchar_fd(str[i], 1);
-		i++;
+			write(1, &*format, 1);
+		if (!format)
+		{
+			va_end(args);
+			return(len);
+		}
+		format++;
+		len++;
 	}
 	va_end(args);
-	return (1);
+	return (len);
 }
--- main.c
+++ main.c
@@ -6,18 +6,32 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 22:26:41 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/05/13 23:30:52 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/05/17 03:00:47 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int test_strlen(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        write(1, &i, 1);
+        i++;
+    }
+    return (i);
+}
+
 int main()
 {
     char *manger = "Hello";
+    char *test = "four";
 
-    ft_printf("manger : %ld\n", ft_strlen(manger));
-    printf("manger : %ld", ft_strlen(manger));
+    int res = printf("%d manger : %d\n", test_strlen(manger), test_strlen(test));
+    int res2 = ft_printf("%d manger : %d\n", test_strlen(manger), test_strlen(test));
+
+    printf("Orignal : %d, ft_printf : %d", res, res2);
 
     return (0); 
 }
